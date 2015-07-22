4f7250f4cc4b6e357f6838e5b67d98ad471f5839
Hugo Iuri <hugo@minutrade.com>
2015-07-22 16:01:07 -0300

    implementando a busca no controle user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,33 +1,41 @@
 (function() {
 
-  var app = angular.module("gitHubRepositories");
+    var app = angular.module("gitHubRepositories");
 
-  var UserController = function($scope, $routeParams, gitHub) {
+    var UserController = function($scope, $routeParams, $location, gitHub) {
 
-    var onRepositoriesComplete = function(data) {
+        var onRepositoriesComplete = function(data) {
 
-        $scope.repos = data;
+            $scope.repos = data;
+
+        };
+
+        var onUserComplete = function(data) {
+
+            $scope.user = data;
+
+            gitHub.getRepositories($scope.user).then(onRepositoriesComplete, onError);
+
+        };
+
+        var onError = function(response) {
+
+            $scope.error = "Could not fetch the data!";
+
+        };
+
+        $scope.search = function(username) {
+
+            $location.path("/" + username);
+
+        };
+
+        $scope.username = $routeParams.username;
+
+        gitHub.getUser($scope.username).then(onUserComplete, onError);
 
     };
 
-    var onUserComplete = function(data) {
-
-      $scope.user = data;
-
-      gitHub.getRepositories($scope.user).then(onRepositoriesComplete, onError);
-
-    };
-
-    var onError = function(response) {
-
-      $scope.error = "Could not fetch the data!";
-
-    };
-
-      gitHub.getUser($routeParams.username).then(onUserComplete, onError);
-
-  };
-
-  app.controller("UserController", ["$scope", "$routeParams", "gitHub", UserController]);
+    app.controller("UserController", ["$scope", "$routeParams", "$location", "gitHub", UserController]);
 
 }());
